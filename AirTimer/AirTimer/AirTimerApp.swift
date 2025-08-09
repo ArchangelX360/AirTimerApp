66c385380f52640bfb0ca6b5e699de3467600f29
ArchangelX360 <titouan.bion@gmail.com>
2025-08-09 11:51:28 +0200

    XCode generation

---
+++ AirTimer/AirTimer/AirTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirTimerApp.swift
+//  AirTimer
+//
+//  Created by Titouan BION on 09/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct AirTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
